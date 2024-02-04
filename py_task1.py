24192d74ac8df1b7e901ecad997ffefa5e05bf1e
rohitmallah01 <rohitnishad852456@gmail.com>
2024-02-04 15:59:19 +0530

    commit1

---
+++ py_task1.py
@@ -0,0 +1,6 @@
+Name = input("Enter your name:")
+Age = input("Enter you age:")
+roll_number = input("Enter your roll Number:")
+Phone_Num = input("Enter your Mobile number:")
+
+print(Name,Age,roll_number,Phone_Num)
